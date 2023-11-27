30c49deb8ce1ad5d6333e663d8b0918d4022c0f3
Jonathan Colton <29485573+jsc17@users.noreply.github.com>
2023-11-27 15:12:08 -0500

    Add read toggle and remove buttons to book card

--- scripts/script.js
+++ scripts/script.js
@@ -1,136 +1,11 @@
-const myLibrary = [
+let myLibrary = [
   {
+    id: 0,
     title: "The Lost Fleet: Dauntless",
     author: "Jack Campbell",
     pages: 304,
     date: 2006,
     read: true,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: true,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: true,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: true,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: true,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: true,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
-  },
-  {
-    title: "The Lost Fleet: Dauntless",
-    author: "Jack Campbell",
-    pages: 304,
-    date: 2006,
-    read: false,
   },
 ];
 
@@ -143,12 +18,17 @@ let read = 0;
 let unread = 0;
 let total = 0;
 
-function Book(title, author, pages, date, read) {
+function Book(id, title, author, pages, date, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.date = date;
   this.read = read;
+}
+
+function generateID() {
+  return myLibrary[myLibrary.length - 1].id + 1;
 }
 
 function createBookCard(book) {
@@ -159,6 +39,7 @@ function createBookCard(book) {
   } else {
     card.classList.add("unread");
   }
+  card.setAttribute("data-id", book.id);
   let cardDetails = document.createElement("div");
   cardDetails.classList.add(".book-details");
   cardDetails.appendChild(document.createElement("h2")).innerHTML = book.title;
@@ -171,10 +52,36 @@ function createBookCard(book) {
   card.appendChild(cardDetails);
   let cardButtons = document.createElement("div");
   cardButtons.classList.add("book-buttons");
-  cardButtons.appendChild(document.createElement("button"));
-  cardButtons.appendChild(document.createElement("button"));
+  tempButton = document.createElement("button");
+  tempButton.innerHTML = "Read";
+  tempButton.addEventListener("click", function (e) {
+    const parent = e.target.parentElement.parentElement;
+    console.log(parent);
+    if (parent.classList.contains("read")) {
+      parent.classList.remove("read");
+      parent.classList.add("unread");
+    } else {
+      parent.classList.add("read");
+      parent.classList.remove("unread");
+    }
+  });
+  cardButtons.appendChild(tempButton);
+  tempButton = document.createElement("button");
+  tempButton.innerHTML = "Remove";
+  tempButton.addEventListener("click", function (e) {
+    const parent = e.target.parentElement.parentElement;
+    removeBook(parseInt(parent.getAttribute("data-id")));
+  });
+  cardButtons.appendChild(tempButton);
   card.appendChild(cardButtons);
   mainLibrary.insertBefore(card, mainLibrary.lastElementChild);
+}
+
+function removeBook(id) {
+  mainLibrary.removeChild(document.querySelector(`[data-id="${id}"]`));
+  myLibrary = myLibrary.filter((book) => {
+    return parseInt(book.id) != id;
+  });
 }
 
 function addBookToLibrary(book) {
@@ -195,6 +102,7 @@ document.querySelectorAll(".show-add").forEach((button) => {
 addBookForm.addEventListener("submit", () => {
   let formData = new FormData(addBookForm);
   let newBook = new Book(
+    generateID(),
     formData.get("title"),
     formData.get("author"),
     formData.get("pages"),
