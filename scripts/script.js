ef748ebc8655851e7891f91125d1757b55fcfaae
Jonathan Colton <29485573+jsc17@users.noreply.github.com>
2023-11-29 22:25:25 -0500

    Refactor to use classes

--- scripts/script.js
+++ scripts/script.js
@@ -1,139 +1,172 @@
-let myLibrary = [];
+class Book {
+  #card = null;
 
-const root = document.documentElement;
-const mainLibrary = document.querySelector(".book-container");
-const modal = document.querySelector("#add-book-modal");
-const addBookForm = document.querySelector("#add-book-form");
-const libraryInfo = document.querySelector(".library-info");
-const filter = document.querySelector("#sort-by");
+  constructor(
+    id,
+    title,
+    author = "unknown",
+    pages = "unknown",
+    date = "unknown",
+    read
+  ) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.date = date;
+    this.isRead = read;
+  }
 
-function Book(id, title, author, pages, date, read) {
-  this.id = id;
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.date = date;
-  this.read = read;
-  this.toggleRead = function () {
-    this.read = !this.read;
+  createCard() {
+    if (this.#card != null) {
+      return this.#card;
+    } else {
+      this.#card = document.createElement("div");
+    }
+    this.#card.classList.add();
+    this.#card.classList.add("book");
+    if (this.isRead) {
+      this.#card.classList.add("read");
+    }
+    this.#card.setAttribute("data-id", this.id);
+
+    let cardDetails = document.createElement("div");
+    cardDetails.classList.add("book-details");
+    cardDetails.appendChild(document.createElement("h2")).innerHTML =
+      this.title;
+    cardDetails.appendChild(document.createElement("p")).innerHTML =
+      "By: " + this.author;
+    cardDetails.appendChild(document.createElement("p")).innerHTML =
+      "Number of pages: " + this.pages;
+    cardDetails.appendChild(document.createElement("p")).innerHTML =
+      "Published: " + this.date;
+    this.#card.appendChild(cardDetails);
+    let tempButton;
+    let cardButtons = document.createElement("div");
+    cardButtons.classList.add("book-buttons");
+    tempButton = document.createElement("button");
+    tempButton.innerHTML = "Read";
+    tempButton.addEventListener("click", function (e) {
+      const id = e.target.parentElement.parentElement.getAttribute("data-id");
+      myLibrary.findBook(id).toggleRead();
+    });
+    cardButtons.appendChild(tempButton);
+    tempButton = document.createElement("button");
+    tempButton.innerHTML = "Remove";
+    tempButton.addEventListener("click", function (e) {
+      const parent = e.target.parentElement.parentElement;
+      myLibrary.removeBook(parseInt(parent.getAttribute("data-id")));
+    });
+    cardButtons.appendChild(tempButton);
+    this.#card.appendChild(cardButtons);
+
+    return this.#card;
+  }
+
+  toggleRead() {
+    this.isRead = !this.isRead;
     let card = document.querySelector(`[data-id="${this.id}"]`);
-    if (this.read) {
+    if (this.isRead) {
       card.classList.add("read");
     } else {
       card.classList.remove("read");
     }
-    updateInfo();
-  };
-}
-
-function generateID() {
-  if (myLibrary.length == 0) {
-    return 0;
+    myLibrary.updateInfo();
   }
-  return myLibrary[myLibrary.length - 1].id + 1;
 }
 
-function createBookCard(book) {
-  let card = document.createElement("div");
+const myLibrary = new (class {
+  #books = [];
+  libraryUI = document.querySelector(".book-container");
+  infoUI = document.querySelector(".library-info");
 
-  //add classes and data-id attribute
+  constructor() {
+    this.addBook(new Book(this.generateID(), "y", "f", 734, 752, true));
+    this.addBook(new Book(this.generateID(), "b", "c", 423, 252, true));
+    this.addBook(new Book(this.generateID(), "a", "z", 123, 152, true));
+    this.addBook(new Book(this.generateID(), "f", "a", 534, 552, true));
 
-  card.classList.add("book");
-  if (book.read) {
-    card.classList.add("read");
+    this.createAddCard();
   }
-  card.setAttribute("data-id", book.id);
 
-  //create title and book detail elements and add them to the card
-  let cardDetails = document.createElement("div");
-  cardDetails.classList.add("book-details");
-  cardDetails.appendChild(document.createElement("h2")).innerHTML = book.title;
-  cardDetails.appendChild(document.createElement("p")).innerHTML =
-    "By: " + book.author;
-  cardDetails.appendChild(document.createElement("p")).innerHTML =
-    "Number of pages: " + book.pages;
-  cardDetails.appendChild(document.createElement("p")).innerHTML =
-    "Published: " + book.date;
-  card.appendChild(cardDetails);
+  generateID() {
+    return this.#books.length == 0
+      ? 0
+      : this.#books[this.#books.length - 1].id + 1;
+  }
 
-  //create read toggle and delete buttons and add them to the card
-  let cardButtons = document.createElement("div");
-  cardButtons.classList.add("book-buttons");
-  tempButton = document.createElement("button");
-  tempButton.innerHTML = "Read";
-  tempButton.addEventListener("click", function (e) {
-    const id = e.target.parentElement.parentElement.getAttribute("data-id");
+  addBook(book) {
+    this.#books.push(book);
+    this.libraryUI.insertBefore(book.createCard(), this.libraryUI.lastChild);
+    this.updateInfo();
+  }
 
-    myLibrary
-      .find((book) => {
-        return book.id == id;
-      })
-      .toggleRead();
-  });
-  cardButtons.appendChild(tempButton);
-  tempButton = document.createElement("button");
-  tempButton.innerHTML = "Remove";
-  tempButton.addEventListener("click", function (e) {
-    const parent = e.target.parentElement.parentElement;
-    removeBook(parseInt(parent.getAttribute("data-id")));
-  });
-  cardButtons.appendChild(tempButton);
-  card.appendChild(cardButtons);
+  removeBook(id) {
+    this.libraryUI.removeChild(document.querySelector(`[data-id="${id}"]`));
+    this.#books = this.#books.filter((book) => {
+      return parseInt(book.id) != id;
+    });
+    this.updateInfo();
+  }
 
-  return card;
-}
+  updateInfo() {
+    let read = 0,
+      unread = 0,
+      total = this.#books.length;
+    this.#books.forEach((book) => {
+      if (book.isRead) {
+        read++;
+      } else {
+        unread++;
+      }
+    });
 
-function removeBook(id) {
-  mainLibrary.removeChild(document.querySelector(`[data-id="${id}"]`));
-  myLibrary = myLibrary.filter((book) => {
-    return parseInt(book.id) != id;
-  });
-  updateInfo();
-}
+    this.infoUI.children[0].innerText = "Read Books: " + read;
+    this.infoUI.children[1].innerText = "Unread Books: " + unread;
+    this.infoUI.children[2].innerText = "Total Books: " + total;
+  }
 
-function updateInfo() {
-  let read = 0,
-    unread = 0,
-    total = myLibrary.length;
-  myLibrary.forEach((book) => {
-    if (book.read) {
-      read++;
-    } else {
-      unread++;
+  findBook(id) {
+    return this.#books.find((book) => {
+      return (book.id = id);
+    });
+  }
+  sort() {
+    this.#books.sort(function (a, b) {
+      let result =
+        a[sortKey] < b[sortKey] ? -1 : a[sortKey] > b[sortKey] ? 1 : 0;
+      return result;
+    });
+
+    this.rebuildLibrary();
+  }
+
+  createAddCard() {
+    let addCard = document.createElement("div");
+    addCard.id = "add";
+    addCard.classList.add("show-add");
+    addCard.addEventListener("click", () => {
+      modal.showModal();
+    });
+    addCard.innerText = "+";
+    this.libraryUI.appendChild(addCard);
+  }
+
+  rebuildLibrary() {
+    while (this.libraryUI.firstChild) {
+      this.libraryUI.removeChild(this.libraryUI.firstChild);
     }
-  });
-
-  libraryInfo.children[0].innerText = "Read Books: " + read;
-  libraryInfo.children[1].innerText = "Unread Books: " + unread;
-  libraryInfo.children[2].innerText = "Total Books: " + total;
-}
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-  mainLibrary.insertBefore(createBookCard(book), mainLibrary.lastElementChild);
-  updateInfo();
-}
-
-function createAddCard() {
-  let addCard = document.createElement("div");
-  addCard.id = "add";
-  addCard.classList.add("show-add");
-  addCard.addEventListener("click", () => {
-    modal.showModal();
-  });
-  addCard.innerText = "+";
-  mainLibrary.appendChild(addCard);
-}
-
-function rebuildLibrary() {
-  while (mainLibrary.firstChild) {
-    mainLibrary.removeChild(mainLibrary.firstChild);
+    this.#books.forEach((book) => {
+      this.libraryUI.appendChild(book.createCard());
+    });
+    this.createAddCard();
   }
-  myLibrary.forEach((book) => {
-    mainLibrary.appendChild(createBookCard(book));
-  });
-  createAddCard();
-}
+})();
+
+const root = document.documentElement;
+const modal = document.querySelector("#add-book-modal");
+const addBookForm = document.querySelector("#add-book-form");
+const filter = document.querySelector("#sort-by");
 
 document.querySelectorAll(".show-add").forEach((button) => {
   button.addEventListener("click", () => {
@@ -144,14 +177,14 @@ document.querySelectorAll(".show-add").forEach((button) => {
 addBookForm.addEventListener("submit", () => {
   let formData = new FormData(addBookForm);
   let newBook = new Book(
-    generateID(),
+    myLibrary.generateID(),
     formData.get("title"),
     formData.get("author"),
     formData.get("pages"),
     formData.get("date"),
     formData.get("read")
   );
-  addBookToLibrary(newBook);
+  myLibrary.addBook(newBook);
   addBookForm.reset();
 });
 
@@ -172,19 +205,5 @@ document.querySelector("#theme-button").addEventListener("click", function () {
 filter.addEventListener("change", function () {
   sortKey = filter.value;
 
-  myLibrary.sort(function (a, b) {
-    let result = a[sortKey] < b[sortKey] ? -1 : a[sortKey] > b[sortKey] ? 1 : 0;
-    return result;
-  });
-
-  console.table(myLibrary);
-
-  rebuildLibrary();
+  myLibrary.sort();
 });
-
-addBookToLibrary(new Book(generateID(), "y", "f", 734, 752, true));
-addBookToLibrary(new Book(generateID(), "b", "c", 423, 252, true));
-addBookToLibrary(new Book(generateID(), "a", "z", 123, 152, true));
-addBookToLibrary(new Book(generateID(), "f", "a", 534, 552, true));
-
-createAddCard();
